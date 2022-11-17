2addf4e39fe16a23be1db907046beb92fd18e457
Christopher Whitley <aristurtledev@gmail.com>
2022-11-16 23:53:57 -0500

    Added new UseSmtp methods and UseFileSystem to IEmailFactoryBuilder interface

--- source/SmtpKit/IEmailFactoryBuilder.cs
+++ source/SmtpKit/IEmailFactoryBuilder.cs
@@ -18,6 +18,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ----------------------------------------------------------------------------- */
+using System.Security.Cryptography.X509Certificates;
+
 using Microsoft.Extensions.DependencyInjection;
 
 using SmtpKit.EmailSenders;
@@ -41,11 +43,67 @@ public interface IEmailFactoryBuilder
     ///     factory to an SMTP server.
     /// </summary>
     /// <param name="host">
-    ///     The IPv4 address or host name of the SMTP server to send the email
-    ///     messages to for delivery.
+    ///     A <see cref="string"/> containing the IPv4 address or host name
+    ///     of the Smtp server to connect to.
     /// </param>
     /// <param name="port">
-    ///     The port to connect with on the <paramref name="host"/>.
+    ///     An <see cref="int"/> value that represents the port to connect to
+    ///     on the <paramref name="host"/>.
     /// </param>
     void UseSmtp(string host, int port);
+
+    /// <summary>
+    ///     Configures the <see cref="IEmailFactory"/> instance being added to
+    ///     the services container to send the email messages created by the
+    ///     factory to an SMTP server.
+    /// </summary>
+    /// <param name="host">
+    ///     A <see cref="string"/> containing the IPv4 address or host name
+    ///     of the Smtp server to connect to.
+    /// </param>
+    /// <param name="port">
+    ///     An <see cref="int"/> value that represents the port to connect to
+    ///     on the <paramref name="host"/>.
+    /// </param>
+    /// <param name="username">
+    ///     A <see cref="string"/> containing the username used to authenticate
+    ///     with the Smtp server.
+    /// </param>
+    /// <param name="password">
+    ///     A <see cref="string"/> containing the password used to authenticate
+    ///     with the Smtp server.
+    /// </param>
+    void UseSmtp(string host, int port, string username, string password);
+
+    /// <summary>
+    ///     Configures the <see cref="IEmailFactory"/> instance being added to
+    ///     the services container to send the email messages created by the
+    ///     factory to an SMTP server.
+    /// </summary>
+    /// <param name="host">
+    ///     A <see cref="string"/> containing the IPv4 address or host name
+    ///     of the Smtp server to connect to.
+    /// </param>
+    /// <param name="port">
+    ///     An <see cref="int"/> value that represents the port to connect to
+    ///     on the <paramref name="host"/>.
+    /// </param>
+    /// <param name="certificate">
+    ///     An <see cref="X509Certificate"/> certificate used to establish
+    ///     a Secure Sockets Layer (SSL) connection when connecting to the
+    ///     Smtp server.  
+    /// </param>
+    void UseSmtp(string host, int port, X509Certificate certificate);
+
+    /// <summary>
+    ///     Configures the <see cref="IEmailFactory"/> instance being added to
+    ///     the services container to send the email messages created by the
+    ///     factory to a text file in the.
+    /// </summary>
+    /// <param name="outDir">
+    ///     A <see cref="string"/> containing the absolute path to the directory
+    ///     to output the email messages in.  If the directory does not exist,
+    ///     it will be created.
+    /// </param>
+    void UseFileSystem(string outDir);
 }
